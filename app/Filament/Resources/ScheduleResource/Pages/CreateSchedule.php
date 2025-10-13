1bfd6b51fc7d017b29409c55efb539df5fbeff32
rmuchamad <kolepz11@gmail.com>
2025-10-13 09:36:52 +0700

    perbaiki klik schedule pada menu data peserta

--- app/Filament/Resources/ScheduleResource/Pages/CreateSchedule.php
+++ app/Filament/Resources/ScheduleResource/Pages/CreateSchedule.php
@@ -6,10 +6,39 @@ use App\Filament\Resources\ScheduleResource;
 use Filament\Actions;
 use Filament\Resources\Pages\CreateRecord;
 use App\Models\Schedule;
+use App\Models\Participant;
 
 class CreateSchedule extends CreateRecord
 {
 	protected static string $resource = ScheduleResource::class;
+
+	public function mount(): void
+	{
+		parent::mount();
+		
+		// Check if participant_id is provided in the URL
+		$participantId = request()->get('participant_id');
+		
+		if ($participantId) {
+			$participant = Participant::find($participantId);
+			
+			if ($participant) {
+				// Pre-fill the form with participant data
+				$this->form->fill([
+					'participant_id' => $participant->id,
+					'nik_ktp' => $participant->nik_ktp,
+					'nrk_pegawai' => $participant->nrk_pegawai,
+					'nama_lengkap' => $participant->nama_lengkap,
+					'tanggal_lahir' => $participant->tanggal_lahir,
+					'jenis_kelamin' => $participant->jenis_kelamin,
+					'skpd' => $participant->skpd,
+					'ukpd' => $participant->ukpd,
+					'no_telp' => $participant->no_telp,
+					'email' => $participant->email,
+				]);
+			}
+		}
+	}
 
 	protected function mutateFormDataBeforeCreate(array $data): array
 	{
